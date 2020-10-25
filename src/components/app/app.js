11c68380f2bcb9d19ad36db61c71f6daee303228
Andrii Stepanets <ovrass@gmail.com>
2020-10-25 20:55:12 +0200

    Added 404 error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,24 +4,26 @@ import MainPage from '../pages';
 import UsersList from '../users-list';
 import AddUser from '../add-user';
 import UserDetails from '../user-details';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () =>  {
     return (
         <>
             <Router>
                 <Header/>
-                <Route path="/"
-                    render={() => <MainPage />}
-                    exact />
-                <Route path="/users-list" exact component={UsersList} />
-                <Route path="/users-list/user/:id" 
-                    render={({ match }) => {
-                        const { id } = match.params;
-                        return <UserDetails userId={id}/>;
-                    }} />
-                <Route path="/add-user" component={AddUser} />
-                
+                <Switch>
+                    <Route path="/"
+                        render={() => <MainPage />}
+                        exact />
+                    <Route path="/users-list" exact component={UsersList} />
+                    <Route path="/users-list/user/:id"
+                        render={({ match }) => {
+                            const { id } = match.params;
+                            return <UserDetails userId={id} />;
+                        }} />
+                    <Route path="/add-user" component={AddUser} />
+                    <Route render={() => <h2 className="text-danger">Something went wrong! Page not found!</h2>} />
+                </Switch>
             </Router>
         </>
     )
